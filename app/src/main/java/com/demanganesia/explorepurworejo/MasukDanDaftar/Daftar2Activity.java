1ce463434b8c188b03a70e1e4d8fd8304966f6af
Kharisma Fajar <restuistyo@gmail.com>
2022-09-10 11:45:03 +0700

    validasi daftar
    Validasi daftar 1 dan 2

--- app/src/main/java/com/demanganesia/explorepurworejo/MasukDanDaftar/Daftar2Activity.java
+++ app/src/main/java/com/demanganesia/explorepurworejo/MasukDanDaftar/Daftar2Activity.java
@@ -4,12 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.demanganesia.explorepurworejo.R;
 
+import java.util.Calendar;
+
 public class Daftar2Activity extends AppCompatActivity {
+
+    RadioGroup radioGrup;
+    RadioButton genderDipilih;
+    DatePicker tanggalLahir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +27,54 @@ public class Daftar2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_daftar2);
         //menghilangkan status bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        radioGrup = findViewById(R.id.radio_grup);
+        tanggalLahir = findViewById(R.id.DPtanggal_lahir);
     }
 
     public void keDaftar3(View view) {
+
+        if(!validasiJenisKelamin() | !validasiUmur()) {
+            return;
+        }
+
+        genderDipilih = findViewById(radioGrup.getCheckedRadioButtonId());
+        String _gender = genderDipilih.getText().toString();
+
+        int day = tanggalLahir.getDayOfMonth();
+        int month = tanggalLahir.getMonth();
+        int year = tanggalLahir.getYear();
+
+        String _date = day+"/"+month+"/"+year;
+
         startActivity(new Intent(getApplicationContext(), Daftar3Activity.class));
     }
 
     public void keMasukb(View view) {
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
+
+    //validasi jenis kelamin
+    private boolean validasiJenisKelamin() {
+        if (radioGrup.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(this, "Pilih salah satu jenis kelamin", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    //validasi umur
+    private boolean validasiUmur() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int userAge = tanggalLahir.getYear();
+        int isAgeValid = currentYear - userAge;
+
+        if(isAgeValid < 10){
+            Toast.makeText(this, "Mohon maaf, kamu belum cukup umur", Toast.LENGTH_SHORT).show();
+            return false;
+        } else
+            return true;
     }
 }
--- app/src/main/java/com/demanganesia/explorepurworejo/MasukDanDaftar/DaftarActivity.java
+++ app/src/main/java/com/demanganesia/explorepurworejo/MasukDanDaftar/DaftarActivity.java
@@ -4,26 +4,121 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.demanganesia.explorepurworejo.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class DaftarActivity extends AppCompatActivity {
+
+    TextInputLayout ETNamaLengkap, ETusername, ETemail, ETKataSandi;
+    Button BtnDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
-
         //menghilangkan status bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    }
 
+        ETNamaLengkap = findViewById(R.id.ETnama_lengkap);
+        ETusername = findViewById(R.id.ETusername);
+        ETemail = findViewById(R.id.ETemail);
+        ETKataSandi = findViewById(R.id.ETkata_sandi);
+        BtnDaftar = findViewById(R.id.btnDaftar);
+
+    }
+    //klik button daftar
     public void keDaftar2(View view) {
-        startActivity(new Intent(getApplicationContext(), Daftar2Activity.class));
+
+        if (!validasiFormNamaLengkap() | !validasiFormUsername() | !validasiFormEmail() | !validasiFormKataSandi()){
+            return;
+        } else {
+            startActivity(new Intent(getApplicationContext(), Daftar2Activity.class));
+        }
     }
 
+    //validasi form nama lengkap
+    private boolean validasiFormNamaLengkap() {
+        String val = ETNamaLengkap.getEditText().getText().toString().trim();
+
+        if (val.isEmpty()) {
+            ETNamaLengkap.setError("Bagian ini tidak boleh kosong");
+            return false;
+        } else {
+            ETNamaLengkap.setError(null);
+            ETNamaLengkap.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    //validasi form username
+    private boolean validasiFormUsername() {
+        String val = ETusername.getEditText().getText().toString().trim();
+        String cekSpasi = "\\A\\w{1,20}\\z";
+
+        if (val.isEmpty()) {
+            ETusername.setError("Bagian ini tidak boleh kosong");
+            return false;
+        } else if(val.length()>20){
+            ETusername.setError("Username terlalu panjang");
+            return false;
+        } else if(!val.matches(cekSpasi)) {
+            ETusername.setError("Spasi tidak diperbolehkan");
+            return false;
+        }
+        else {
+            ETusername.setError(null);
+            ETusername.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    //validasi form email
+    private boolean validasiFormEmail() {
+        String val = ETemail.getEditText().getText().toString().trim();
+        String cekEmail = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (val.isEmpty()) {
+            ETemail.setError("Bagian ini tidak boleh kosong");
+            return false;
+        } else if(!val.matches(cekEmail)) {
+            ETemail.setError("Masukkan format email dengan benar");
+            return false;
+        }
+        else {
+            ETemail.setError(null);
+            ETemail.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    //validasi form kata sandi
+    private boolean validasiFormKataSandi() {
+        String val = ETKataSandi.getEditText().getText().toString().trim();
+        String cekKataSandi = "^"+
+                "(?=.*[a-zA-Z])"+   //semua karakter diperbolehkan
+                "(?=\\S+$)"+        //tidak boleh ada spasi
+                ".{4,}"+            //harus berisi minimal 4 karakter
+                "$";
+
+        if (val.isEmpty()) {
+            ETKataSandi.setError("Bagian ini tidak boleh kosong");
+            return false;
+        } else if(!val.matches(cekKataSandi)) {
+            ETKataSandi.setError("Tidak boleh ada spasi dan minimal berisi 4 karakter");
+            return false;
+        }
+        else {
+            ETKataSandi.setError(null);
+            ETKataSandi.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    //klik ke masuk
     public void keMasuk(View view) {
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
